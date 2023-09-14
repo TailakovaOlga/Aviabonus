dccd5b498f24e8446e1265076a591be2bb79d428
Olga <Taylakova_Olga@mail.ru>
2023-09-14 16:41:45 +0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int initialCheck = 100; // Начальный счет
+        int replenishment = 300; //Первое пополнение счета
+        int secondbill = 1100; // Второе пополнение счета
+        int bonus = 100; // Количество рублей для одного бонуса
+        int finalbonus = initialCheck + replenishment + secondbill; // Итоговый бонус
+
+        if (finalbonus > 1000) {
+            System.out.println("Итоговый бонус положительный");
+        }
+
+
+        System.out.println("Итоговая сумма счета:");
+        System.out.println((initialCheck + replenishment + secondbill) + " рублей");
+        System.out.println("Итоговый бонус:");
+        System.out.println((finalbonus / bonus) + " бонусов");
+
+    }
 }
+
+
+
+
+
